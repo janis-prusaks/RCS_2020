a8ca1bc5c9c90f482b56f4c1470d393eca47515a
janis-prusaks <janim.prusakam@gmail.com>
2020-10-28 12:36:19 +0200

    Add project files.

---
+++ Day1_N/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Day1_N
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
